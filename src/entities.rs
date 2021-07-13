d38338d48f0b3166806c52470bdab478d73022ed
bl0rch1d <temaflash96@gmail.com>
2021-07-14 02:01:00 +0300

    Add entities

---
+++ src/entities.rs
@@ -0,0 +1,30 @@
+pub struct Author {
+    pub name: String,
+    pub bio: String
+}
+
+pub struct Book {
+    pub author: Author,
+    pub title: String
+}
+
+pub struct Reader {
+    pub name: String,
+    pub email: String,
+    pub city: String,
+    pub street: String,
+    pub house: String
+}
+
+pub struct Order {
+    pub book: Book,
+    pub reader: Reader,
+    pub date: String
+}
+
+pub struct Library {
+    pub authors: Vec<Author>,
+    pub books: Vec<Book>,
+    pub readers: Vec<Reader>,
+    pub orders: Vec<Order>,
+}
