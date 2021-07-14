32f4d61e64e709c14bfa977b94b34b9fff819638
Artyom Dizhevskyi <38437488+bl0rch1d@users.noreply.github.com>
2021-07-15 00:11:16 +0300

    Implement storage logic (#6)

--- src/main.rs
+++ src/main.rs
@@ -4,22 +4,24 @@ mod book;
 mod reader;
 mod order;
 mod library;
+mod storage;
 
 use entities::*;
 use library::Entity;
+use storage::Storage;
 
 
 fn main() {
-    let author = Author::new("Steven King".to_string(), "Some bio for author".to_string());
-    let book = Book::new("Steven King".to_string(), &author);
-    let reader = Reader::new(
-        "Fool".to_string(),
-        "yofool@mail.com".to_string(),
-        "Foolish city".to_string(),
-        "Foolish street".to_string(),
-        "Fool's house".to_string()
-    );
-    let order = Order::new(&book, &reader, "12.12.2600".to_string());
+    // let author = Author::new("Steven King".to_string(), "Some bio for author".to_string());
+    // let book = Book::new("Steven King".to_string(), &author);
+    // let reader = Reader::new(
+    //     "Fool".to_string(),
+    //     "yofool@mail.com".to_string(),
+    //     "Foolish city".to_string(),
+    //     "Foolish street".to_string(),
+    //     "Fool's house".to_string()
+    // );
+    // let order = Order::new(&book, &reader, "12.12.2600".to_string());
 
     // println!("{}", author.to_s());
     // println!("{}", book.to_s());
@@ -27,13 +29,15 @@ fn main() {
     // println!("{}", order.to_s());
 
     let mut library = Library::new(vec![], vec![], vec![], vec![]);
-    library.add_entity(Entity::Author(&author));
-    library.add_entity(Entity::Author(&author));
-    library.add_entity(Entity::Book(&book));
-    library.add_entity(Entity::Order(&order));
-    library.add_entity(Entity::Reader(&reader));
-    library.add_entity(Entity::Reader(&reader));
-    library.add_entity(Entity::Reader(&reader));
+    // library.add_entity(Entity::Author(&author));
+    // library.add_entity(Entity::Author(&author));
+    // library.add_entity(Entity::Book(&book));
+    // library.add_entity(Entity::Order(&order));
+    // library.add_entity(Entity::Reader(&reader));
+    // library.add_entity(Entity::Reader(&reader));
+    // library.add_entity(Entity::Reader(&reader));
+    library.load_data();
+
     println!("{:?}", library.authors);
     println!("\n============\n");
     println!("{:?}", library.books);
@@ -41,4 +45,7 @@ fn main() {
     println!("{:?}", library.orders);
     println!("\n============\n");
     println!("{:?}", library.readers);
+
+    // library.save_data();
+    // library.load_data();
 }
